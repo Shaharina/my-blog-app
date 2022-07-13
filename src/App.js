0ae28c1ad151f857324d9b819a3b4b66a85d097a
Shaharina Mita <S.mita90232@gmail.com>
2022-07-13 22:19:52 +0600

    style added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <LoadPosts></LoadPosts>
+            <District name='Noakhali' special='Pitha'></District>
+            <District name='Dhaka' special='Beef'></District>
+            <District name='Khulna' special='Jhal'></District>
+        </div>
+    );
+}
+
+function LoadPosts() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => setPosts(data));
+    }, []);
+    return (
+        <div>
+            <h1>Posts:{posts.length} </h1>
+            {posts.map(post => (
+                <Post title={post.title} body={post.body}></Post>
+            ))}
+        </div>
+    );
+}
+function Post(props) {
+    return (
+        <div style={{ backgroundColor: 'lightsalmon', margin: '20px ', border: '4px solid salmon', borderRadius: '20px' }}>
+            <h2>Title: {props.title}</h2>
+            <p>Body: {props.body}</p>
+        </div>
+    );
+}
+
+function District(props) {
+    const [power, setPower] = useState(1);
+
+    const boostPower = () => {
+        const newPower = power * 2;
+        setPower(newPower);
+    };
+    return (
+        <div className='District'>
+            <h2>Name:{props.name}</h2>
+            <p>Specialty:{props.special}</p>
+            <h4>Power{power} </h4>
+            <button onClick={boostPower}>Boost The Power</button>
+        </div>
+    );
 }
 
 export default App;
